7df8860698198183055363e75fe02d23c27f9c4c
hmkne36 <minhkhangyadea0241@gmail.com>
2026-03-08 13:46:22 +0700

    Create tiki.js

---
+++ api/accesstrade/coupon-site/tiki.js
@@ -0,0 +1,55 @@
+const searchParams = new URL(window.location.href);
+const ctv_id = searchParams.searchParams.get('ref');
+var atScript6626 = document.getElementById('atScript6626');
+if(ctv_id && ctv_id !== null && ctv_id.length >= 1 && ctv_id != ''){
+	console.log(ctv_id);
+	if(ctv_id.match(/[0-9]{6}/g)){
+		atScript6626.setAttribute('data-utm-source','ctv');
+		atScript6626.setAttribute('data-utm-medium',`ctv_${ctv_id}`);
+		atScript6626.setAttribute('data-utm-campaign',`CuongbokIT CTV: ${window.location.pathname}`);
+	}else{
+		atScript6626.setAttribute('data-utm-source','ref');
+		atScript6626.setAttribute('data-utm-medium',`ref_${ctv_id}`);
+		atScript6626.setAttribute('data-utm-campaign',`CuongbokIT REF: ${window.location.pathname}`);
+	}
+}
+
+var updateTime = new Date();
+var today = `${updateTime.getDate()}.${updateTime.getMonth() + 1}`;
+document.querySelectorAll('.postTitle')[0].innerHTML += `${today} `;
+
+
+
+
+window.addEventListener('DOMContentLoaded', (event) => {
+	console.log('DOM fully loaded and parsed');
+
+	var filterSort = document.querySelectorAll('.atEQPOIVFSDFSDG-filter-sort')
+	filterSort[1].style.display = 'none';
+	
+	
+	setTimeout(function(){
+		var voucherLen = document.querySelectorAll('.atEQPOIVFSDFSDG-nav-item > .atEQPOIVFSDFSDG-nav-link > em');
+		var totalVoucher = parseInt(replaceBrackets(voucherLen[0].innerText)) + parseInt(replaceBrackets(voucherLen[1].innerText));
+		document.querySelector('.atEQPOIVFSDFSDG-list-title > h4').innerHTML = `HIỆN CÓ <span style='color:red;margin: 0px;'>${totalVoucher.toLocaleString()}</span> MÃ <!--span style='color:red;margin: 0px;'>(${add0To1Length(updateTime.getHours())}:${add0To1Length(updateTime.getMinutes())} ${today})</span-->:`;
+	},3000)
+	
+
+	const pageItem = document.querySelectorAll('.atEQPOIVFSDFSDG-pagination > .atEQPOIVFSDFSDG-page-item');
+	pageItem.forEach((element) => {element.addEventListener("click", couponTab);});
+});
+
+
+document.querySelectorAll('[name="viewport"]')[0].setAttribute('content','width=device-width, initial-scale=1, user-scalable=1, minimum-scale=1, maximum-scale=1');
+
+function couponTab() {
+	location.hash = "";
+	location.hash = "second-block";
+}
+function replaceBrackets(c){
+	return c.replaceAll(/\(|\)/g,'');
+}
+function add0To1Length(c){
+	if(c < 10 || parseInt(c) < 10 || c.toString().length == 1){c = parseInt(`0${c}`)}
+	return c.toString().padStart(2, '0')
+}
